5efd45cbb4c6833efdae5eed60bf3d7304b72248
unknown <raphael.tavares@javalidigital.com.br>
2014-11-02 22:43:14 -0200

    Deploy v1 - Prototype

---
+++ wp-content/themes/oldpaper/searchform.php
@@ -0,0 +1,3 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+    <div><label class="screen-reader-text" for="s">Pesquisar...</label><input placeholder="Pesquisar..." type="text" value="" name="s" id="s" /><button type="submit" id="searchsubmit"><i class="fa fa-search"></i></button></div>
+</form>
